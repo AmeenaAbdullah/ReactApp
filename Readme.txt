5d4ccffe135b0edd803881be91e3038c69fe5db8
Ameena <99495118+AmeenaAbdullah@users.noreply.github.com>
2023-03-10 22:41:02 +0500

    first commit

---
+++ Readme.txt
@@ -0,0 +1,31 @@
+1. Make sql connection
+2. create backend folder
+      D:\react\cruds> cd backend
+      D:\react\cruds\backend> npm init -y
+3. install express
+      D:\react\cruds\backend> npm i express mysql nodemon
+
+4 Make index.js file 
+
+5. fetch data from the backend
+use library 
+->npm i axios
+
+
+6 to access daTA FROM BACKEND(WHEN YOUR APP NOT ALLOWING TO USE OUR BACKEND API)
+
+->NPM I CORS
+
+
+
+
+
+
+
+  <BrowserRoute> 
+      <Routes>
+        <Route path="/" element={<Book/>}>     </Route>
+        <Route path="/" element={<Add/>}>     </Route>
+        <Route path="/" element={<Update/>}>     </Route>
+      </Routes>
+      </BrowserRoute>
---
+++ backend/index.js
@@ -0,0 +1,79 @@
+import express from "express";
+import mysql from "mysql";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "test",
+});
+
+app.get("/", (req, res) => {
+  res.json("hello");
+});
+
+app.get("/book", (req, res) => {
+  const q = "SELECT * FROM book";
+  db.query(q, (err, data) => {
+    if (err) {
+      console.log(err);
+      return res.json(err);
+    }
+    return res.json(data);
+  });
+});
+
+//ADD BOOK
+app.post("/book", (req, res) => {
+  const q = "INSERT INTO book(`title`, `desc`, `price`, `cover`) VALUES (?)";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [values], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+//DELETE BOOK
+app.delete("/book/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = " DELETE FROM books WHERE id = ? ";
+
+  db.query(q, [bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+//UPDATE
+app.put("/book/:id", (req, res) => {
+  const bookId = req.params.id;
+  const q = "UPDATE book SET `title`= ?, `desc`= ?, `price`= ?, `cover`= ? WHERE id = ?";
+
+  const values = [
+    req.body.title,
+    req.body.desc,
+    req.body.price,
+    req.body.cover,
+  ];
+
+  db.query(q, [...values,bookId], (err, data) => {
+    if (err) return res.send(err);
+    return res.json(data);
+  });
+});
+
+app.listen(8800, () => {
+  console.log("Connected to backend.");
+});
